a59cd72a0dd05b4fa26f94f9cecca5199439ec09
bryanwweber <bryan.w.weber@gmail.com>
2019-06-28 11:43:27 -0400

    Update Windows build scripts
    Applies to libcantera and Python package. Fix various options specified to cantera.conf. Make sure the script exits if one of the commands errors.

--- cantera/build_py.bat
+++ cantera/build_py.bat
@@ -1,6 +1,11 @@
-@ECHO ON
+echo ****************************
+echo PYTHON BUILD STARTED
+echo ****************************
 SET "ESC_PYTHON=%PYTHON:\=/%"
-CALL scons build python_package=y python_cmd="%ESC_PYTHON%"
+ECHO python_package='full' >> cantera.conf
+ECHO python_cmd="%ESC_PYTHON%" >> cantera.conf
+CALL scons build
 IF ERRORLEVEL 1 EXIT 1
 cd interfaces/cython
 "%PYTHON%" setup.py build --build-lib=../../build/python install
+IF ERRORLEVEL 1 EXIT 1

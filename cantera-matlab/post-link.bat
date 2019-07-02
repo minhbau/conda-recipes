d9032545ac43c217d49892d8cf9c526d4400ac50
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-07-02 15:26:57 -0400

    Add post-link and pre-unlink for *nix
    Also modify the scripts to not include the Cantera version in the
    startup.m file, since it would become out-of-date if the package version
    is updated. Add the conda environment where the package is being
    installed so multiple environments can have the package installed
    without overwriting each other.

--- cantera-matlab/post-link.bat
+++ cantera-matlab/post-link.bat
@@ -1,17 +1,20 @@
-:: This script adds the appropriate setup lines to startup.m
-:: This script will edit files in the user's Documents directory,
-:: in direct contravention of the guideline from Conda to not
-:: edit any files outside the install prefix. This seems like the
-:: easiest way to set these variables though; the alternative is
-:: to make people type/add these lines themselves. /shrug
+REM This script adds the appropriate setup lines to startup.m
+REM This script will edit files in the user's Documents directory,
+REM in direct contravention of the guideline from Conda to not
+REM edit any files outside the install prefix. This seems like the
+REM easiest way to set these variables though; the alternative is
+REM to make people type/add these lines themselves. /shrug
 @ECHO off
 SET "MATLAB_STARTUP=%USERPROFILE%\Documents\MATLAB\startup.m"
 
-:: If the startup.m file already exists, delete any lines added by this script
-IF EXIST "%MATLAB_STARTUP%" COPY "%MATLAB_STARTUP%" "%MATLAB_STARTUP%.cantera.bak"
-IF EXIST "%MATLAB_STARTUP%.cantera.bak" FINDSTR /V "%% added by Cantera %PKG_VERSION% Conda Installer" "%MATLAB_STARTUP%.cantera.bak" > "%MATLAB_STARTUP%"
+REM Set a variable with the name of the conda environment
+for %%A in ("%PREFIX%\") do set CONDA_ENV=%%~nxA
 
-:: Add Cantera lines to the startup.m script
+REM If the startup.m file already exists, delete any lines added by this script
+IF EXIST "%MATLAB_STARTUP%" COPY "%MATLAB_STARTUP%" "%MATLAB_STARTUP%.cantera.bak"
+IF EXIST "%MATLAB_STARTUP%.cantera.bak" FINDSTR /V "%% added by Cantera Conda Installer for %CONDA_ENV% environment" "%MATLAB_STARTUP%.cantera.bak" > "%MATLAB_STARTUP%"
+
+REM Add Cantera lines to the startup.m script
 ECHO. >> "%MATLAB_STARTUP%"
-ECHO setenv('CANTERA_DATA', '%PREFIX%\Library\cantera\data') %% added by Cantera %PKG_VERSION% Conda Installer>> "%MATLAB_STARTUP%"
-ECHO addpath(genpath('%PREFIX%\Library\lib\cantera\matlab')) %% added by Cantera %PKG_VERSION% Conda Installer>> "%MATLAB_STARTUP%"
+ECHO setenv('CANTERA_DATA', '%PREFIX%\Library\cantera\data') %% added by Cantera Conda Installer for %CONDA_ENV% environment>> "%MATLAB_STARTUP%"
+ECHO addpath(genpath('%PREFIX%\Library\lib\cantera\matlab')) %% added by Cantera Conda Installer for %CONDA_ENV% environment>> "%MATLAB_STARTUP%"
--- cantera-matlab/pre-unlink.bat
+++ cantera-matlab/pre-unlink.bat
@@ -1,6 +1,9 @@
 @ECHO off
 SET "MATLAB_STARTUP=%USERPROFILE%\Documents\MATLAB\startup.m"
 
-:: If the startup.m file already exists, delete any lines added by this script
+REM Set a variable with the name of the conda environment
+for %%A in ("%PREFIX%\") do set CONDA_ENV=%%~nxA
+
+REM If the startup.m file already exists, delete any lines added by this script
 IF EXIST "%MATLAB_STARTUP%" COPY "%MATLAB_STARTUP%" "%MATLAB_STARTUP%.cantera.bak"
-IF EXIST "%MATLAB_STARTUP%.cantera.bak" FINDSTR /V "%% added by Cantera %PKG_VERSION% Conda Installer" "%MATLAB_STARTUP%.cantera.bak" > "%MATLAB_STARTUP%"
+IF EXIST "%MATLAB_STARTUP%.cantera.bak" FINDSTR /V "%% added by Cantera Conda Installer for %CONDA_ENV% environment" "%MATLAB_STARTUP%.cantera.bak" > "%MATLAB_STARTUP%"
